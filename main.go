8f3a4437550ab825767d51af877863aaf1a596fe
Jesse Molina <jesse@jessemolina.xyz>
2022-03-22 16:19:30 -0500

    basic show and create snippet handlers

--- main.go
+++ main.go
@@ -7,7 +7,18 @@ import (
 
 // define a home hanlder function
 func home(w http.ResponseWriter, r *http.Request) {
+	w.Header()
 	w.Write([]byte("Hello Snippet!"))
+}
+
+// show specific snippet
+func showSnippet(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("show specific snippet"))
+}
+
+// create a snippet
+func createSnippet(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("create a snippet"))
 }
 
 func main() {
@@ -16,6 +27,8 @@ func main() {
 
 	// register the home function as the hanlder for root url
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	// listen and server on 4000
 	log.Println("Starting server on :4000")
